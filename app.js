df6930efe062a8d08e7774764b1254e4478b552e
fpalorma <fedepalorma@gmail.com>
2023-12-02 22:42:59 -0300

    se agrego loop

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ function convertir(pesos){
 alert(pesos+" Pesos serian "+conversion+" Fichas de Tomy y Daly")
 }}
 
+function iniciarConversion(){
+    if(confirmacion == "si"){
+        let pesos = parseFloat(prompt("Ingrese cuántos pesos desea convertir a Fichas de Tomy y Daly"));
+    convertir(pesos)
+    
+    } else if (confirmacion == null){
+        alert("Gracias, vuelva prontos")
+    }else{
+        alert("Gracias, vuelva prontos")
+    };
+}
 let confirmacionInicial = prompt("Desea simular una conversión de moneda? (si/no)")
 let confirmacion = confirmacionInicial ? confirmacionInicial.toLowerCase(): confirmacionInicial = "no";
-console.log(confirmacion);
-
-if(confirmacion == "si"){
-    let pesos = parseFloat(prompt("Ingrese cuántos pesos desea convertir a Fichas de Tomy y Daly"));
-convertir(pesos)
-
-} else if (confirmacion == null){
-    alert("Gracias, vuelva prontos")
-}else{
-    alert("Gracias, vuelva prontos")
-}
+let repetir = "";
+do{
+    iniciarConversion()
+    repetir = prompt("desea realizar otra conversión? (si/no)")
+}while(repetir == "si")
